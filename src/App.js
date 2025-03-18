68b5c7f083b7aadd6f1f18fd15b777d44e62a04d
jaegwae <kimjk4031@naver.com>
2025-03-18 22:04:51 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Link, useNavigate, Outlet, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet, BrowserRouter, HashRouter } from 'react-router-dom';
 import { useState } from "react";
 import Home from './routes/Home.jsx';
 import Result from './routes/Result.jsx';
@@ -10,13 +10,13 @@ function App() {
   let [InputText, setInputText] = useState("");
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Home InputText={InputText} setInputText={setInputText} />} />
           <Route path='/result' element={<Result InputText={InputText} setInputText={setInputText}/>} />
         </Routes>
 
-      </BrowserRouter>
+      </HashRouter>
 
     </div>
 
